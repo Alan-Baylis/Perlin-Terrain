c84d7c011b4806af2b55754aaf009bab43be28fa
Mircea Catana <mircea.catalin.catana@gmail.com>
2016-03-17 23:17:56 +0000

    Project setup + Basic Perlin

---
+++ Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapDisplay : MonoBehaviour {
+
+    public Renderer textureRenderer;
+
+    public void DrawNoiseMap(float[,] noiseMap) {
+        int width = noiseMap.GetLength(0);
+        int height = noiseMap.GetLength(1);
+
+        Color[] colorMap = new Color[width * height];
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                colorMap[width * y + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+            }
+        }
+
+        Texture2D texture = new Texture2D(width, height);
+        texture.SetPixels(colorMap);
+        texture.Apply();
+
+        textureRenderer.sharedMaterial.mainTexture = texture;
+        textureRenderer.transform.localScale = new Vector3(width, 1.0f, height);
+    }
+
+}
---
+++ Assets/Scripts/Noise.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Noise {
+
+    public static float[,] GenerateNoiseMap(int width, int height, float scale) {
+        float[,] noiseMap = new float[width, height];
+
+        if (scale <= 0.0f) {
+            scale = 0.0001f;
+        }
+
+        for (int y = 0; y < height; ++y) {
+            for (int x = 0; x < width; ++x) {
+                float sampleX = x / scale;
+                float sampleY = y / scale;
+
+                noiseMap[x, y] = Mathf.PerlinNoise(sampleX, sampleY);
+
+            }
+        }
+
+        return noiseMap;
+    }
+
+
+}
